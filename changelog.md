2cfb2ecf922e5b9f77a3b8d4d27118794dfc9afb
Nathan Menge <nathan.menge@gmail.com>
2024-07-09 14:30:51 -0400

    [CI] Configuring testing job to fail without 100% coverage.  Also fixing code coverage - go figure.
    [Changelog] Back filling missing entries
    [Version check] Adding change log to version check script
    [Version] Semantic versions no longer have a 'v'

--- changelog.md
+++ changelog.md
@@ -1,5 +1,22 @@
 # Changelog
 
+## 1.1.3
+- Fixed code coverage and added failure to pytest CI job if code coverage is not 100%
+- Brought changelog up to date
+- Enhanced version check script to check version from changelog as well
+- Moved release versioning to not include a 'v' in version numbers
+
+## 1.1.2
+- Added support for default values when a collection is not focusable
+- Added support for tuples, which really became a bug fix for the top level collection being anything other than a dict
+- Improved version check script to include checking pyproject.toml
+
+## 1.1.1
+- Fixed issue when indexing an empty list
+- Moved focus and FocusingError to package level
+- Updated Github actions
+- Updated minimum python to 3.11
+
 ## 1.1.0
 - Fixed exception thrown when key sets finds an empty collection to be a FocusingError with explanation rather than an IndexError
 
--- version_check.sh
+++ version_check.sh
@@ -1,14 +1,16 @@
 set -e
 
 function check {
-  if [ "$1" = "$2" ] && [ "$2" = "$3" ]; then
+  if [ "$1" = "$2" ] && [ "$2" = "$3" ] && [ "$3" = "$4" ]; then
     exit 0
   else
     echo "Release notes file should have the topmost release match the tagged semantic version"
     echo "pyproject.toml should have the version set to match the README and the tagged version being published"
+    echo "changelog.md should have a new entry for the newly tagged release"
     echo "Extracted from release notes $1"
     echo "CI_COMMIT_TAG = $2"
     echo "Extracted from pyproject.toml $3"
+    echo "Extracted from changelog.md $4"
     exit 1
   fi
 }
@@ -18,9 +20,11 @@ function get_readme_version {
 }
 
 function get_project_version {
-  # Please note this function cheats a little and adds a v, because we EXPECT the version tags to include v
-  # This will have to be changed if I stop using tags with a v.
-  cat $1 | head -n4 | tail -n1 | awk '{print $3}' | sed 's/"//g' | sed 's/^/v/g'
+  cat $1 | head -n4 | tail -n1 | awk '{print $3}' | sed 's/"//g'
 }
 
-check "$(get_readme_version README.md)" "$GITHUB_REF_NAME" "$(get_project_version pyproject.toml)"
+function get_change_log_version {
+  cat $1 | head -n3 | tail -n1 | awk '{print $2}'
+}
+
+check "$(get_readme_version README.md)" "$GITHUB_REF_NAME" "$(get_project_version pyproject.toml)" "$(get_change_log_version changelog.md)"
